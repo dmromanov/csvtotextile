51aebd52b21d3061410c11c8014080c7c4eff891
Dmitrii Romanov <dmromanov@users.noreply.github.com>
2017-08-24 00:31:15 +0300

    Ensure mbstring to behave exactly on all platforms

--- bin/csvtotextile.php
+++ bin/csvtotextile.php
@@ -4,6 +4,8 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 use CsvToTextile\Command\CsvToTextileCommand;
 use Symfony\Component\Console\Application;
 
+mb_internal_encoding('utf-8');
+
 $command = new CsvToTextileCommand();
 $application = new Application('CsvToTextile', '1.0.0');
 
